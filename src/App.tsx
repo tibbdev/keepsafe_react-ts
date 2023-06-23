e82661a6730b430722b92482a72c837dc8f10754
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-06-23 12:01:58 +0100

    React + Typescript examples

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 import './App.css';
 
 import ProjectsPage from './projects/ProjectsPage';
+import Hello from './Hello';
 
-function App() {
+// function Greeter(props: {first: string, last?: string}) {
+//    const { first, last } = props;
+//    return (
+//       <h1>
+//          Hello, {first} {last}
+//       </h1>
+//    );
+// }
+
+const App: React.FC = () => {
    return (
-      <div className='container'>
-         <ProjectsPage />
+      <div className="App">
+         <Hello name="David" enthusiasmLevel={2}/>
+         {/* <ProjectsPage /> */}
       </div>
    );
 }
