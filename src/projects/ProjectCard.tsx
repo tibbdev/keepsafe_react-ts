8fa854d3967d80fb1b4a2f1eaf860ceb5324c400
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-06-26 14:09:15 +0100

    Lab 11 :  Child to Parent Communication

--- src/projects/ProjectCard.tsx
+++ src/projects/ProjectCard.tsx
@@ -7,13 +7,16 @@ function formatDescription(description: string): string {
 
 interface ProjectCardProps {
     project: Project;
+    onEdit: (project: Project) => void;
 }
 
 function ProjectCard(props: ProjectCardProps) {
-    const { project } = props;
+    const { project ,
+            onEdit
+        } = props;
 
     const handleEditClick = (projectbeingEdited: Project) => {
-        console.log("EDIT: ", projectbeingEdited);
+        onEdit(projectbeingEdited);
     }
 
     return (
--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -9,11 +9,16 @@ interface ProjectListProps {
 }
 
 function ProjectList({ projects }: ProjectListProps) {
+  const handleProjectEditClick = (project: Project) =>
+  {
+    console.log(project)
+  }
+
   return (
     <div className="row">
       {projects.map((project) => (
         <div key={project.id} className="cols-sm">
-          <ProjectCard project={project} />
+          <ProjectCard project={project} onEdit={handleProjectEditClick} />
           <ProjectForm />
         </div>
       ))}
