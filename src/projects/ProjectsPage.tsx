07bb0e84e2d28618ae57d3c8bcb19f98ebf0d52e
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-06-26 12:13:09 +0100

    Lab 8 : Making 'ProjectCard' Component

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -6,28 +6,8 @@ import { Project } from "./Project";
 
 function ProjectsPage()
 {
-    const temp_project = new Project({
-        id: 1,
-        name: 'Johnson - Kutch',
-        description:
-            'Fully-configurable intermediate framework. Ullam occaecati libero laudantium nihil voluptas omnis.',
-        imageUrl: '/assets/placeimg_500_300_arch4.jpg',
-        contractTypeId: 3,
-        contractSignedOn: '2013-08-04T22:39:41.473Z',
-        budget: 54637,
-        isActive: false,
-        });
-    
     return (
         <>
-            {
-                console.log(temp_project)
-            }
-            {/* {
-                MOCK_PROJECTS.map((project) => {
-                    console.log(project);
-                })
-            } */}
             <h1>Projects</h1>
             <ProjectList projects={MOCK_PROJECTS} />
         </>
