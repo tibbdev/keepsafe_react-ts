8b82998bf66dec62c8fb7f1726104e7f1b33a467
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-07-03 23:00:49 +0100

    Lab 20 complete : Added Navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,18 @@ import HomePage from './home/HomePage';
 const App: React.FC = () => {
    return (
       <Router>
+         <header className='sticky'>
+            <span className='logo'>
+               <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+            </span>
+            <NavLink to='/' className='button rounded'>
+               <span className='icon-home'></span>
+               Home
+            </NavLink>
+            <NavLink to='/projects' className='button rounded'>
+               Projects
+            </NavLink>
+         </header>
          <div className='container'>
             <Routes>
                <Route path='/' element={<HomePage />} />
