c192eccfea9dfe0f05d4c59b2e03d1af7c4a1c21
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-07-03 22:54:00 +0100

    added Routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import './App.css';
 
 import ProjectsPage from './projects/ProjectsPage';
-
+import HomePage from './home/HomePage';
 
 const App: React.FC = () => {
    return (
-      <div className="App">
-         <ProjectsPage />
-      </div>
+      <Router>
+         <div className='container'>
+            <Routes>
+               <Route path='/' element={<HomePage />} />
+               <Route path='/projects' element={<ProjectsPage />} />
+            </Routes>
+         </div>
+      </Router>
+      
    );
 }
 
