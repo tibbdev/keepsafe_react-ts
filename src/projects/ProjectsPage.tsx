a8031762594c7795326c516f586d240e42479cb4
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-07-03 15:39:26 +0100

    Lab 18 : Adding data from backend json server

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,4 +1,5 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState , useEffect} from "react";
+import { projectAPI } from "./projectAPI";
 import { MOCK_PROJECTS } from "./MockProjects";
 import ProjectList from "./ProjectList";
 // import '../App.css'
@@ -6,7 +7,35 @@ import { Project } from "./Project";
 
 function ProjectsPage()
 {
-    const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+    const [projects, setProjects] = useState<Project[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | undefined>(undefined);
+
+    useEffect(() => 
+    {
+        async function loadProjects() 
+        {
+            setLoading(true);
+            try
+            {
+                const data = await projectAPI.get(1);
+                setError('');
+                setProjects(data);
+            }
+            catch (e)
+            {
+                if (e instanceof Error)
+                {
+                    setError(e.message);
+                }
+            }
+            finally
+            {
+                setLoading(false);
+            }
+        }
+        loadProjects();
+    }, []);
 
     const SaveProject = (project: Project) => {
         console.log('Saving Project: ', project);
@@ -19,7 +48,29 @@ function ProjectsPage()
     return (
         <Fragment>
             <h1>Projects</h1>
+            {
+                error && (
+                <div className="row">
+                    <div className="card large error">
+                        <section>
+                            <p>
+                            <span className="icon-alert inverse "></span>
+                            {error}
+                            </p>
+                        </section>
+                    </div>
+                </div>
+                )
+            }
             <ProjectList projects={projects} onSave={SaveProject}/>
+            {
+                loading && (
+                    <div className="center-page">
+                        <span className="spinner primary"></span>
+                        <p>Loading...</p>
+                    </div>
+                )
+            }
         </Fragment>
     );
 }
